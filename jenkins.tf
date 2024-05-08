5ebcb47c5b842290250baa4495b1241ed73968c9
dbha <dbha0719@gmail.com>
2024-05-08 11:06:41 +0900

    update

--- jenkins.tf
+++ jenkins.tf
@@ -1,18 +1,18 @@
-# Install jenkins chart 
+# # Install jenkins chart 
 
-resource "helm_release" "jenkins" {
-  name       = "jenkins"
-  repository = "https://charts.jenkins.io"
-  chart      = "jenkins"
-  version    = var.jenkins_helm_version
+# resource "helm_release" "jenkins" {
+#   name       = "jenkins"
+#   repository = "https://charts.jenkins.io"
+#   chart      = "jenkins"
+#   version    = var.jenkins_helm_version
 
-  namespace        = var.jenkins_namespace
-  create_namespace = true
+#   namespace        = var.jenkins_namespace
+#   create_namespace = true
 
-  values = [file("jenkins_values.yaml")]
+#   values = [file("jenkins_values.yaml")]
 
-  depends_on = [
-    helm_release.ingress_nginx,
-    helm_release.metallb,
-  ]
-}
+#   depends_on = [
+#     helm_release.ingress_nginx,
+#     helm_release.metallb,
+#   ]
+# }
